152c1ffc374ec7a7eca1ccef897b699ce357c14a
CaelanYoung2000 <100617735+CaelanYoung2000@users.noreply.github.com>
2022-02-28 14:11:18 -0600

    Update Lab7-1.cpp

--- #4/Lab7-1.cpp
+++ #4/Lab7-1.cpp
@@ -1,6 +1,6 @@
 //Lab7-1.cpp - calculates the average number of text
 //messages sent each day for 7 days
-//Created/revised by <your name> on <current date>
+
 
 #include <iostream>
 #include <iomanip>
