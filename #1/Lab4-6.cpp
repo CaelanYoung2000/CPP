cf1f5df37951fa53e5bfbe7dbccc2da1a8af83dd
CaelanYoung2000 <100617735+CaelanYoung2000@users.noreply.github.com>
2022-02-28 13:53:27 -0600

    Add files via upload

---
+++ #1/Lab4-6.cpp
@@ -0,0 +1,18 @@
+//Lab4-6.cpp - displays the area of a triangle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//declare variables
+	int base = 11;
+	int height = 13;
+	double area = 0.0;
+
+	//calculate and display area
+	area = base * height / 2;
+	cout << "Area: " << area << endl;
+
+	return 0;
+}	//end of main function
